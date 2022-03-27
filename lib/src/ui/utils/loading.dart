5b4ef7eeefc91014d73e4149e79571f6709b119b
baguspanji <bagusp0518@gmail.com>
2022-03-27 22:12:05 +0700

    fix null safety

--- lib/src/ui/utils/loading.dart
+++ lib/src/ui/utils/loading.dart
@@ -1,40 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:progress_dialog/progress_dialog.dart';
+import 'package:sn_progress_dialog/sn_progress_dialog.dart';
 
 class Dialogs {
   static ProgressDialog? pr;
   static Future showLoading(BuildContext context) async {
-    pr = new ProgressDialog(context);
-    pr = new ProgressDialog(context,
-        type: ProgressDialogType.Normal, isDismissible: true, showLogs: false);
-    pr!.style(
-        message: 'Tunggu Sebentar...',
-        borderRadius: 10.0,
-        backgroundColor: Colors.black45,
-        progressWidget: Lottie.asset(
-          'assets/loading.json',
-          width: 200,
-          height: 200,
-          fit: BoxFit.fill,
-        ),
-        elevation: 10.0,
-        insetAnimCurve: Curves.easeInOut,
-        progress: 0.0,
-        maxProgress: 100.0,
-        progressTextStyle: TextStyle(
-            color: Colors.black, fontSize: 12.0, fontWeight: FontWeight.w400),
-        messageTextStyle: TextStyle(
-            color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.w600));
-    return pr!.show();
+    pr = ProgressDialog(context: context);
+    // pr = ProgressDialog(context: context,
+    //     type: ProgressDialogType.Normal, isDismissible: true, showLogs: false);
+    pr!.show(
+      max: 100,
+      msg: 'Tunggu Sebentar...',
+      borderRadius: 10.0,
+      backgroundColor: Colors.black45,
+      elevation: 10.0,
+
+      // progressWidget: Lottie.asset(
+      //   'assets/loading.json',
+      //   width: 200,
+      //   height: 200,
+      //   fit: BoxFit.fill,
+      // ),
+    );
   }
 
   static Future dismiss(BuildContext context) async {
-    pr = new ProgressDialog(context);
-    pr = new ProgressDialog(context,
-        type: ProgressDialogType.Normal, isDismissible: true, showLogs: false);
-    return pr!.hide().whenComplete(() {
-      print("hide success");
-    });
+    pr = ProgressDialog(context: context);
+    pr!.close();
+    // pr = new ProgressDialog(context,
+    //     type: ProgressDialogType.Normal, isDismissible: true, showLogs: false);
+    // return pr!.hide().whenComplete(() {
+    //   print("hide success");
+    // });
   }
 }
